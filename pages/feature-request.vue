ea9f468102f3203f0d553f2eedb32799cb008e47
Josee <info@joseewouters.nl>
2019-04-28 13:47:42 +0200

    Changes to form for Netlify

--- pages/feature-request.vue
+++ pages/feature-request.vue
@@ -1,7 +1,8 @@
 <template>
     <main class="main">
         <h1 class="title">Feature request</h1>
-        <form name="request" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+        <form name="request" action="/pages/thanks" method="POST" netlify-honeypot="bot-field" data-netlify="true">
+            <input type="hidden" name="form-name" value="request" />
             <fieldset class="hidden">
                 <label for="bot-field">Don't fill this in if you're human</label>
                 <input name="bot-field">
@@ -10,12 +11,18 @@
             <label for="Name">
                 Your name:
             </label>
-             <label>Your Email: <input type="email" name="email" /></label>
             <input type="text" name="name">
+
+            <label>
+                Your Email: 
+            </label>
+            <input type="email" name="email" />
+
             <label for="message">
                 Your request:
             </label>
             <textarea name="message"></textarea>
+
             <button type="submit">Submit</button>
         </form>
     </main>
---
+++ pages/thanks.vue
@@ -0,0 +1,15 @@
+<template>
+    <main class="main">
+        <h1 class="title">Thanks!</h1>
+    </main>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
