59c7ffb2c5c89cb59caa7eed750ab5d5673d6730
Josee <info@joseewouters.nl>
2019-04-29 11:36:06 +0200

    Add about page

---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1 class="title">About What Dinner?</h1>
+        <article class="article">
+            <h2>Once upon a time...</h2>
+            <p>There were two developers sitting at the dinner table and looking at each other: "What should we have for dinner?"</p>
+            <p>"I don't know," said the first one after a while.</p>
+            <p>"Maybe we should put a pizza in the oven," the other said hesistantly.</p>
+            <p>And they did. While they were waiting for the pizza to be ready, the first developer said: "We should develop an app for this! Let it choose what we should eat."</p>
+            <h2>So I did</h2>
+            <p>I'm Josee Wouters and I developed What Dinner? to help me decide what I should eat.</p>
+            <p>Now you can download and try What Dinner too. Hopefully it will make your life, or at least your dinner decision making process, a little bit easier too.</p>
+        </article>
+    </div>
+</template>
--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="container">
         <h1 class="title">Privacy Policy</h1>
-        <div class="content">
+        <article class="article">
             <h2>
                 Data storage
             </h2>
             <p>
-                The meals and ingredients you put in What Dinner? are saved in the local storage of your device. This means that no one else has access to your data. It also means your meals cannot be restored when you switch phones or when you accidentally delete your meals.
+                The meals and ingredients you put in What Dinner? are saved in the local storage of your device. This means that What Dinner has no access to your data. It also means your meals cannot be restored when you switch phones or when you accidentally delete your meals.
             </p>
             <h2>
                 Access
@@ -22,20 +22,7 @@
             <p>
                 What Dinner? doesn't ask personal data of you. You do not have to create an account to use the app, you remain anonymous when using What Dinner?. 
             </p>
-        </div>
+        </article>
     </div>
 </template>
-
-<style lang="scss">
-.content {
-    max-width: 700px;
-    width: 100%;
-    margin-top: 1em;
-
-    h2 {
-        margin-top: 1em;
-        margin-bottom: .2em;
-    }
-}
-</style>
 
